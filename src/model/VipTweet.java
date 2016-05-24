80c9c3c5a0eb3ebac62f54703a72e82e82b11df7
Michael <michael.waelischmiller@gmx.de>
2016-05-24 15:08:18 +0200

    VipTweets Table extended

--- src/model/VipTweet.java
+++ src/model/VipTweet.java
@@ -2,6 +2,59 @@ package model;
 
 public class VipTweet {
 	
+	private int authorId;
+	private String authorName;
+	private String idStr;
+	private int inReplyTo;
+	private int[] mentions;
+	private int retweetOrigin;
+	private String text;
+	
+	
+	
+	public int getAuthorId() {
+		return authorId;
+	}
+	public void setAuthorId(int authorId) {
+		this.authorId = authorId;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	public String getIdStr() {
+		return idStr;
+	}
+	public void setIdStr(String idStr) {
+		this.idStr = idStr;
+	}
+	public int getInReplyTo() {
+		return inReplyTo;
+	}
+	public void setInReplyTo(int inReplyTo) {
+		this.inReplyTo = inReplyTo;
+	}
+	public int[] getMentions() {
+		return mentions;
+	}
+	public void setMentions(int[] mentions) {
+		this.mentions = mentions;
+	}
+	public int getRetweetOrigin() {
+		return retweetOrigin;
+	}
+	public void setRetweetOrigin(int retweetOrigin) {
+		this.retweetOrigin = retweetOrigin;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
 	
 
 }
