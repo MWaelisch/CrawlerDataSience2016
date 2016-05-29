8b97250ec74553608e5724d2052e26d803bad0e0
Michael <michael.waelischmiller@gmx.de>
2016-05-29 19:49:11 +0200

    Improve sentistrength wrapper

--- src/postProcessing/SentiStrengthWrapper.java
+++ src/postProcessing/SentiStrengthWrapper.java
@@ -4,18 +4,17 @@ import uk.ac.wlv.sentistrength.*;
 
 public class SentiStrengthWrapper {
 	
-	public SentiStrengthWrapper() {}
+	private SentiStrength sentiStrength;
 	
-	public void testSenti(){
-
-		//Method 2: One initialisation and repeated classifications
-		SentiStrength sentiStrength = new SentiStrength(); 
+	public SentiStrengthWrapper() {
+		this.sentiStrength = new SentiStrength(); 
 		//Create an array of command line parameters to send (not text or file to process)
 		String ssthInitialisation[] = {"sentidata", "SentiData/sentistrength_de/"};
 		sentiStrength.initialise(ssthInitialisation);
-		System.out.println(sentiStrength.computeSentimentScores("Ich hasse frösche.")); 
+	}
+	
+	public void testSenti(){
+		System.out.println(sentiStrength.computeSentimentScores("Das wetter ist schön.")); 
 		System.out.println(sentiStrength.computeSentimentScores("I liebe hunde.")); 
 	}
-
-
 }
